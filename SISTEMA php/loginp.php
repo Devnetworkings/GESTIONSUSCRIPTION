fb8ec2437816d31e987d723b1c8a898d7105878b
lovertetas <willymosquera211@gmail.com>
2026-06-09 22:57:07 -0400

    Delete SISTEMA php directory

--- SISTEMA php/loginp.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name= $_POST['operator'];
-$password= $_POST['password'];
-
-echo $name." ". $password;
-
-?>
-
-<a href="dashboard.php">hii</a>
--- SISTEMA php/logout.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-session_start();
-session_destroy(); // ¡Corte! Matamos la memoria de la sesión
-header("Location: Index.php"); // Te devolvemos a la taquilla
-exit;
-?>
